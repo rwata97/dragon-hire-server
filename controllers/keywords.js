16bcab0de40bf86735ec612523b06f84731b9f18
Ruth <ruth.wata@unosquare.com>
2023-08-01 16:15:14 -0500

    add new route and controller

--- controllers/keywords.js
+++ controllers/keywords.js
@@ -1,15 +1,15 @@
-const serviceData = require("../middlewares/serviceData");
+const serviceData = require('../middlewares/serviceData');
 module.exports = {
   postKeywords: async (req, res) => {
     try {
       const payload = req.body;
 
-      const keywords = ["javascript", "nodejs", "express", "java", "sql"];
+      const keywords = ['javascript', 'nodejs', 'express', 'java', 'sql'];
 
       res.status(200).json({ keywords });
     } catch (err) {
       console.error(err);
-      res.status(500).json({ message: "Server error" });
+      res.status(500).json({ message: 'Server error' });
     }
   },
 
@@ -22,7 +22,19 @@ module.exports = {
       res.status(200).json({ filteredCandidates });
     } catch (err) {
       console.error(err);
-      res.status(500).json({ message: "Server error" });
+      res.status(500).json({ message: 'Server error' });
+    }
+  },
+
+  getLinkedInStatus: async (req, res) => {
+    try {
+      setTimeout(() => {
+        res.status(200).json({
+          status: true,
+        });
+      }, 10000);
+    } catch (error) {
+      console.log(error);
     }
   },
 };
--- routes/keywords.js
+++ routes/keywords.js
@@ -3,5 +3,6 @@ const keywordsController = require('../controllers/keywords');
 
 router.post('/', keywordsController.postKeywords);
 router.post('/candidates', keywordsController.postCandidates);
+router.get('/importlinkedin', keywordsController.getLinkedInStatus);
 
 module.exports = router;
